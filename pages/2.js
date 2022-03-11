d8f68c92116b5f455bf5f57f1479f7497842d0f8
Clarise Nicole Smith <36603779+Fillygroove@users.noreply.github.com>
2022-03-11 14:47:06 -0500

    Archived Volume 2

---
+++ pages/2.js
@@ -0,0 +1,4 @@
+let data = {
+    name: "Pokemon Purple Version - Vol. 2",
+    pics: 135
+};
--- pages/main.js
+++ pages/main.js
@@ -3,12 +3,17 @@ let data = {
         name: 'Main',
         link: '../'
     }],
+    title: `Pokemon Purple Archive`,
     main: [
-        `Hello world!`,
-        `This is a storage website for an old collection of comics I made. I've only archived the first one out of the ones I have so far.`,
-        `Some of the old comics feature my deadname as opposed to my real name. I'm sharing unedited versions purely for historical reasons.`,
+        `Hello world! This is a storage website for an old collection of comics I made. I've only archived the first one out of the ones I have so far.`,
         `I'm missing volumes 3 through volume 6, part 1. Those volumes were lost when my aunt and uncle had gotten their new car almost a full decade ago. If you have these comics it would be appreciated if you could share them!!`,
+        `Some of the old comics feature my deadname as opposed to my real name. I'm sharing unedited versions purely for historical reasons.`,
         `I have the following volumes uploaded:`,
-        `<a href="?p=1">Vol. 1</a>`
+        () => {
+            addList([
+                `<a href="?p=1">Vol. 1</a>`,
+                `<a href="?p=2">Vol. 2</a>`
+            ])
+        }
     ]
 };
